38a0d09da123456eaf5d699041232b49443aca84
hchrist2010 <christhu@oregonstate.edu>
2021-04-07 01:56:59 -0700

    Moved the expansion to despatch function

--- assignment1/dispatch.py
+++ assignment1/dispatch.py
@@ -2,8 +2,22 @@ from searchMethods.astar import astar
 from searchMethods.bfs import bfs
 from searchMethods.dfs import dfs
 from searchMethods.iddfs import iddfs
+from Node import Node
+
 
 def dispatch(start, goal, mode, outFile):
+    leftBank = start[0]
+    rightBank = start[1]
+
+    n = Node(0, leftBank, rightBank)
+    n.expandChildren()
+    n.printNode()
+    n.printchildren()
+    print()
+    n.validate()
+    n.printNode()
+    n.printchildren()
+    print()
     if(mode == 'astar'):
         astar(start, goal)
     elif(mode == 'bfs'):
