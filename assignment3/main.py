3ae65b76a268bc1e31cf1a793a0e509a35cd58ce
Hchri <Hchri@DESKTOP-57HFART>
2021-05-24 11:35:40 -0700

    finished pre-processing

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,34 +1,95 @@
 import re
+import os
 import bisect
 
-testSet = open("testSet.txt", "rt")
-trainingSet = open("trainingSet.txt", "rt")
-punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+testFile = open("testSet.txt", "rt")
+trainingFile = open("trainingSet.txt", "rt")
 
-def getVocab(set):
+def parseFile(file):
     lines = []
-    vocabulary = []
+    vocab = set()
 
-    for line in set:
+    for line in file:
         line = line.strip()
         line = line.split('\t')
         line[0] = line[0].strip()
         line[0] = re.sub(r'[^\w | \s]', '', line[0])
         line[0] = line[0].lower()
         line[0] = line[0].split()
-        for word in line[0]:
-            bisect.insort(vocabulary, word)
+        # for word in line[0]:
+        #     vocab.add(word)
         line[1] = line[1].strip()
         line[1] = int(line[1])
-
         lines.append(line)
+    return lines
 
-    # vocabulary = list(set(vocabulary))
+def getVocab(lines):
+    vocab = set()
+    for line in lines:
+        for word in line[0]:
+            vocab.add(word)
+    return sorted(vocab)
 
-    # print(lines)
-    return vocabulary
 
-print(list(set(getVocab(trainingSet))))
+# class feature:
+#     def __init__(self, size):
+#         self.size = size
+#         self.feats = [0] * (size + 1)
+#
+#     def setFeatures(selfself, sentence):
 
-# vocabulary = list(set(getVocab(trainingSet)))
-# vocabulary.sort()
+def createFeature(vocab, lines):
+    M = len(vocab)
+
+    feature = [0] * (M + 1)
+    for word in lines[0][0]:
+        for i in range(len(vocab)):
+            if word == vocab[i]:
+                feature[i] = 1
+
+    features = []
+    featureLen = 0
+    for line in lines:
+        features.append([0] * (M + 1))
+        for word in line[0]:
+            for i in range(M):
+                if(word == vocab[i]):
+                    features[featureLen][i] = 1
+        if line[1] == 1:
+            features[featureLen][M] = 1
+        featureLen += 1
+
+    return features
+
+def outputPreProcessed(vocab, filePath, features):
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+    out = open(filePath, "w")
+
+    for word in vocab:
+        out.write('%s,'% word)
+    out.write('classlabel')
+    out.write('\n')
+
+    for feature in features:
+        for feat in feature:
+            out.write('%d,' % feat)
+        out.write('\n')
+
+
+    # if os.path.exists("preprocessed_train.txt"):
+    #     os.remove("preprocessed_train.txt")
+    # if os.path.exists("preprocessed_test.txt"):
+    #     os.remove("preprocessed_test.txt")
+
+trainingLines = parseFile(trainingFile)
+testLines = parseFile(testFile)
+
+trainingVocab = getVocab(trainingLines)
+
+trainingFeatures = createFeature(trainingVocab, trainingLines)
+testFeatures = createFeature(trainingVocab, testLines)
+
+outputPreProcessed(trainingVocab, "preprocessed_train.txt", trainingFeatures)
+outputPreProcessed(trainingVocab, "preprocessed_test.txt", testFeatures)
